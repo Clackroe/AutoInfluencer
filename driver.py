30c0a117c9d1df99b95a2d155cdfae2a1e63be81
Clackroe <clackroe@icloud.com>
2023-06-17 17:47:06 -0400

    Added errors when needed

--- driver.py
+++ driver.py
@@ -7,6 +7,14 @@ from tts import getTTS
 from editer import editMovie
 import utils
 
+if (not os.path.exists('assets')):
+    os.mkdir('assets')
+if (not os.path.exists('gameplay-clips')):
+    os.mkdir('gameplay-clips')
+    raise Exception("Please put gameplay clips in the gameplay-clips folder. Clips should be .mp4, and should be at least 90 seconds long. (Preferably 5 minutes or longer.))")
+if (not os.path.exists('env.py')):
+    raise Exception("Could not locate the 'env.py'. This is most likely due to it still being named 'env[REMOVEME].py'. Please rename it to 'env.py' and fill in the required fields. ")
+
 
 subreddit = "AskReddit"
 
--- editer.py
+++ editer.py
@@ -1,6 +1,7 @@
 import os, random
 
-from moviepy.editor import VideoFileClip, AudioFileClip, TextClip, CompositeVideoClip
+from moviepy.editor import VideoFileClip, AudioFileClip, TextClip, CompositeVideoClip, ImageClip
+# from moviepy.video.fx.resize import resize
 from moviepy.video.tools.subtitles import SubtitlesClip
 
 def editMovie(path):
@@ -10,34 +11,41 @@ def editMovie(path):
     
     clipLength = int(clip.duration)
     
-    audio = AudioFileClip(path + 'speech.mp3')
-    
-    audioLength = int(audio.duration)
-    
-    os.system(f'autosub {path}/speech.mp3')
-    
-    
-    generator = lambda txt: TextClip(txt, font='Bernard-MT-Condensed', fontsize=40,  color='white', stroke_color='black', stroke_width=1.5, bg_color='transparent', method='caption', align='center', size=(clip.w, clip.h))
-    subs = SubtitlesClip(path + 'speech.srt', generator)
-    
-    subtitles = SubtitlesClip(subs, generator)
-    
-    
-    
    
     
     
     
+    audio = AudioFileClip(path + 'speech.mp3')
     
+    audioLength = int(audio.duration)
+    
+    image = ImageClip(f'{path}/post.png').to_ImageClip().margin(bottom=350).set_start(0).set_duration(audioLength).set_position(('center', 'bottom'))
+    
+    image = image.resize((image.w * 1.5, image.h * 1.5))
+    
+    os.system(f'autosub {path}/speech.mp3')
+    
+    
+    generator = lambda txt: TextClip(txt, font='Cooper Black', fontsize=95,  color='white', bg_color='transparent', method='caption', align='center', size=(clip.w, clip.h)).set_position(('center', 0.75))
+    subs = SubtitlesClip(path + 'speech.srt', generator)
+    
+    subtitles = SubtitlesClip(subs, generator)
+    
+        
     endPoint = random.randint(audioLength+5, clipLength-5)
     
     backgroundClip = clip.subclip(int(endPoint - audioLength), endPoint)
     
     mainClip = backgroundClip.set_audio(audio)
     
-    mainClip = CompositeVideoClip([mainClip, subtitles.set_position(('center', 0.6))])
+    mainClip = CompositeVideoClip([mainClip, image, subtitles], use_bgclip=True)
     
-    mainClip.write_videofile(path + 'final.mp4')
+    #Currently not working 100%. The image crops out the video below it
+    
+    # mainClip.resize(width=1280, height=720)
+    
+    
+    mainClip.write_videofile(path + 'final.mp4', threads=8, fps=24)
     
     
     
